4986e61711127d1eb7b59a4ec0b05df528ae49b2
camillapalombi <camilla.palombi14@gmail.com>
2022-02-22 16:43:59 +0100

    CHIUSURA HAMBURGER MENU

--- main.js
+++ main.js
@@ -5,10 +5,14 @@ let linkClear = document.querySelector('.close');
 let hamburgerMenu = document.querySelector('.hamburger-menu');
 
 /*Event listener*/
-
+    /*Apertura*/
 linkGenerate.addEventListener('click' , function(){
     let hamMenu = hamburgerMenu.value;
 
     hamburgerMenu.style.display = "block";
     
+})
+    /*Chiusura*/
+linkClear.addEventListener('click' , function() {
+    hamburgerMenu.style.display = "none";
 })
